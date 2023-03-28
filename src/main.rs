ed35b9a61b07c02bae589dd3375db610f51e68f6
Hon Chow <honfungchow@gmail.com>
2023-03-29 00:14:14 +0800

    Added dictionary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::io::{stdin, BufRead, BufReader};
+use std::fs::File;
+use std::str::Split;
+
 fn main() {
-    println!("Hello, world!");
+    // Initialize the word library
+    read_ref();
+
+    let mut s = String::new();
+    println!("Please type or paste what you want to check");
+
+    stdin().read_line(&mut s).expect("Please check if the input is a string");
+    println!("You have entered {s}.");
+    
+    let sub_s: Split<&str> = s.split(" ");
+    println!("Your input contains the following words:");
+    for i in sub_s {
+        println!("{i}");
+    }
+}
+
+fn read_ref() -> Vec<String> {
+    let file = File::open("ref.txt")
+        .expect("File not found, please check if the file \"ref.txt\" exists.");
+    let reader = BufReader::new(file);
+
+    let sub_s: Vec<String> = reader
+        .lines()
+        .map(|s| s.unwrap())
+        .collect();
+
+    return sub_s
 }
